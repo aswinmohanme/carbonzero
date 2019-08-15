882414a94307626aa865bd845e2425411bbd6559
aswinmohanme <dev.aswinmohan@gmail.com>
2019-08-15 08:43:26 +0530

    Format to json from xml

--- lib/services/calculator_api.dart
+++ lib/services/calculator_api.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:xml2json/xml2json.dart';
 
 class CalculatorApi {
   static const String BASE_URL = 'coolclimate.berkeley.edu';
   static const String API_URL = '/calculators/household/api.php';
-
   static const String APP_TOKEN = 'DPP6zZDkSQAlRKfQ-iWgdg';
 
   static var _httpClient = new http.Client();
@@ -11,8 +13,15 @@ class CalculatorApi {
   static getDefaultsAndResults(location) async {
     var uri = new Uri.https(
         BASE_URL, API_URL, {"op": "get_defaults_and_results", ...location});
-    var response = await _httpClient.get(uri,
+    var xmlResponse = await _httpClient.get(uri,
         headers: {"X-DEV-TOKEN": APP_TOKEN, "Accept": "applicaton/xml"});
-    print(response.body);
+    return parseXmlToJson(xmlResponse);
+  }
+
+  static parseXmlToJson(xmlData) {
+    var xml2json = new Xml2Json();
+    xml2json.parse(xmlData.body);
+    var jsondata = xml2json.toGData();
+    return json.decode(jsondata);
   }
 }
--- test/calculator_api_test.dart
+++ test/calculator_api_test.dart
@@ -4,12 +4,14 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group("Calculator Api", () {
     test('get default values and results', () async {
-      await CalculatorApi.getDefaultsAndResults({
+      var defaults_and_results = await CalculatorApi.getDefaultsAndResults({
         "input_location": "06704",
         "input_location_mode": "1",
         "input_income": "1",
         "input_size": "3"
       });
+      expect(defaults_and_results["response"]["input_location"]["\$t"], '6704');
+      expect(defaults_and_results["response"]["input_size"]["\$t"], '3');
     });
   });
 }
