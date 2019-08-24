6f32163efb8325b65d4cd676a4e60d8ff7c2a962
aswinmohanme <dev.aswinmohan@gmail.com>
2019-08-24 15:14:29 +0530

    Move Routing logic to Component

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:carbon/locator.dart';
 import 'package:carbon/ui/components/no_scroll_glow.dart';
 import 'package:carbon/ui/router.dart';
-import 'package:carbon/ui/screens/home_screen.dart';
-import 'package:carbon/ui/screens/news_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:carbon/ui/themes.dart';
-import 'package:typicons_flutter/typicons.dart';
 
 void main() {
   setupLocator();
   runApp(App());
 }
 
-class App extends StatefulWidget {
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  int currentScreenIndex = 1;
-  final screens = [HomeScreen(), HomeScreen(), NewsScreen()];
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,31 +21,7 @@ class _AppState extends State<App> {
       },
       theme: buildCarbonZeroThemeFromBrightness(
           Theme.of(context).brightness, Theme.of(context)),
-      home: Scaffold(
-        body: screens[currentScreenIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: currentScreenIndex,
-          backgroundColor: Theme.of(context).primaryColor,
-          selectedItemColor: Theme.of(context).backgroundColor,
-          unselectedItemColor: Theme.of(context).backgroundColor,
-          showSelectedLabels: false,
-          onTap: (int index) {
-            setState(() {
-              currentScreenIndex = index;
-            });
-          },
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Typicons.infinity), title: Text("Feed")),
-            BottomNavigationBarItem(
-                backgroundColor: Theme.of(context).backgroundColor,
-                icon: Icon(Typicons.compass),
-                title: Text("Home")),
-            BottomNavigationBarItem(
-                icon: Icon(Typicons.news), title: Text("News")),
-          ],
-        ),
-      ),
+      home: Router(),
     );
   }
 }
--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -1,24 +1,42 @@
 import 'package:carbon/ui/screens/home_screen.dart';
 import 'package:carbon/ui/screens/news_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:typicons_flutter/typicons.dart';
 
-class Router {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(
-            builder: (BuildContext context) => HomeScreen());
-      case '/news':
-        return MaterialPageRoute(
-            builder: (BuildContext context) => NewsScreen());
-      default:
-        return MaterialPageRoute(
-            builder: (_) => Scaffold(
-                  body: Center(
-                    child: Text("No Route Defined"),
-                  ),
-                ));
-    }
+class Router extends StatefulWidget {
+  @override
+  _RouterState createState() => _RouterState();
+}
+
+class _RouterState extends State<Router> {
+  int currentScreenIndex = 1;
+  final screens = [HomeScreen(), HomeScreen(), NewsScreen()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentScreenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentScreenIndex,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).backgroundColor,
+        unselectedItemColor: Theme.of(context).backgroundColor,
+        showSelectedLabels: false,
+        onTap: (int index) {
+          setState(() {
+            currentScreenIndex = index;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Typicons.infinity), title: Text("Feed")),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).backgroundColor,
+              icon: Icon(Typicons.compass),
+              title: Text("Home")),
+          BottomNavigationBarItem(
+              icon: Icon(Typicons.news), title: Text("News")),
+        ],
+      ),
+    );
   }
 }
