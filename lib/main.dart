7a6ad0d747d1f0ee96932026fb4855b0655ef634
aswinmohanme <dev.aswinmohan@gmail.com>
2019-08-24 08:57:07 +0530

    Remove homescreen glow

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:carbon/locator.dart';
+import 'package:carbon/ui/components/no_scroll_glow.dart';
 import 'package:carbon/ui/router.dart';
 import 'package:flutter/material.dart';
 import 'package:carbon/ui/themes.dart';
@@ -12,6 +13,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: NoScrollGlow(),
+          child: child,
+        );
+      },
       theme: buildCarbonZeroThemeFromBrightness(
           Theme.of(context).brightness, Theme.of(context)),
       onGenerateRoute: Router.generateRoute,
