2fc66c42d9b6112ad21edf00688e75f37d1d6ee1
aswinmohanme <dev.aswinmohan@gmail.com>
2019-08-24 14:51:15 +0530

    Add launch on click

--- lib/ui/screens/news_screen.dart
+++ lib/ui/screens/news_screen.dart
@@ -34,7 +34,7 @@ class _NewsScreenState extends State<NewsScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                padding: EdgeInsets.only(bottom: s_4),
+                padding: EdgeInsets.only(bottom: s_6),
                 child: Text(
                   "News",
                   style: textTheme.display2,
@@ -48,14 +48,22 @@ class _NewsScreenState extends State<NewsScreen> {
                     RssItem newsItem = climateNewsStore.newsItems[index];
                     return GestureDetector(
                       onTap: () async {
-                        await launch(newsItem.source.url);
+                        await launch(newsItem.link);
                       },
                       child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: s_4),
+                        padding: const EdgeInsets.symmetric(vertical: s_6),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Text(newsItem.title, style: textTheme.display1),
+                            Container(
+                              padding: EdgeInsets.only(top: s_1),
+                              child: Text(
+                                newsItem.link,
+                                style: textTheme.subtitle,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
                           ],
                         ),
                       ),
